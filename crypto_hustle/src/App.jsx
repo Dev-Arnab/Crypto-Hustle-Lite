8886c599838919680fe36a96cecd1240153302d1
Dev Jyoti Ghosh Arnab <devjyotighosharnab@gmail.com>
2025-04-02 23:47:35 -0400

    Solved ssue with git guardian

--- crypto_hustle/src/App.jsx
+++ crypto_hustle/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
-console.log(API_KEY);
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null)
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <></>
